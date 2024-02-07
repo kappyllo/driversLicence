5da6445d5b6af4b9ec1f43b0973bcd848dda1e57
kappyllo <33810546+kappyllo@users.noreply.github.com>
2024-02-07 09:54:07 +0100

    Repairing small bug witch link in the footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,13 @@ export default function Footer() {
   return (
     <div className="flex justify-center">
       <p className="text-sm absolute bottom-16 text-gray-500">
-        Coded by{" "}
+        Coded by
         <a
           href="https://github.com/kappyllo"
           className="mr-1 ml-1 text-gray-700"
         >
           github/kappyllo.
-        </a>{" "}
+        </a>
         Website uses react.
       </p>
     </div>
